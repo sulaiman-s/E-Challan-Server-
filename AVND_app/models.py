5c6a13f1b534cc73cacb8fef9ef4fedd0e2562e0
Sulaiman <sulaimanfarooqi526@gmail.com>
2021-11-26 21:31:21 +0500

    created migrations  and mssql database

--- AVND_app/models.py
+++ AVND_app/models.py
@@ -8,14 +8,14 @@ class Challan(models.Model):
     challan_choices = [("Bi", "Bike"), ("Ca", "Car"),
                        ("Tr", "Truck"), ("Bu", "Bus")]
     status_choices = [("P", "Paid"), ("NP", "Not Paid")]
-    challan_no = models.CharField(primary_key=True, max_length=100)
-    vehicle_number = models.CharField(unique=True)
+    challan_no = models.CharField(primary_key=True, max_length=255)
+    vehicle_number = models.CharField(unique=True, max_length=255)
     vehicle_type = models.CharField(
-        choices=challan_choices, default="Bike", max_length=1)
-    challan_amount = models.CharField()
+        choices=challan_choices, default="Bike", max_length=255)
+    challan_amount = models.CharField(max_length=255)
     challan_time = models.DateTimeField(auto_now=True)
     challan_location = models.TextField()
-    challan_status = models.CharField(choices=status_choices)
+    challan_status = models.CharField(choices=status_choices, max_length=255)
 
 
 class Queries(models.Model):

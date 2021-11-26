f71f37c99472e51a6fb49592a37390fbc2a9ec38
Sulaiman <sulaimanfarooqi526@gmail.com>
2021-11-26 22:21:56 +0500

    customized admin.py

--- Challans/admin.py
+++ Challans/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Challan
 # Register your models here.
+
+admin.site.register(Challan)
--- Queries/admin.py
+++ Queries/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.Queries)
+admin.site.register(models.Uploads)
